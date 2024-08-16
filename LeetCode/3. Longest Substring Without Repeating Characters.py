10a3e1699b14ec0805fd6e0eb64baec8ec3be265
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2024-08-16 19:34:02 +0300

    Update 3. Longest Substring Without Repeating Characters.py

--- LeetCode/3. Longest Substring Without Repeating Characters.py
+++ LeetCode/3. Longest Substring Without Repeating Characters.py
@@ -3,19 +3,17 @@ import re
 
 class Solution(object):
     # high time complexity
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if len(s) == 0:
-            return 0
-        a = []
-        result = 0
-        for t in range(0,len(s) + 1):
-            for i in range(0,len(s) + 1):
-                if s[t:i] not in a and s[t:i] != "":
-                    a.append(s[t:i])
-        a = filter(lambda a: len(set(a)) == len(a),a)
-        return len(sorted(a,key=len)[-1])
-        
+    
+    def lengthOfLongestSubstring(s):
+            if len(set(s)) == len(s):                       # Checking if the string is unique
+                return len(s)
+            substring = ''
+            maxLen = 1
+            for i in s:
+                if i not in substring:                      # If given character is not in substring we are generating, we add it to the substring
+                    substring = substring + i
+                    maxLen = max(maxLen, len(substring))    # We update the maxlength if our substring is longer than the current max.
+                else:                                       # Else, if the character is already part of the substring, we need to break the flow
+                    substring = substring.split(i)[1] + i   # And create a new substring from that point onwards and this new substring is build from the next character. Process repeats till the loop ends.
+            return maxLen
+            
