96f97da76bfe1ef804227ecf6f4ae5d2e60d0772
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-02-21 08:29:50 +0000

    Merge Two Sorted Lists

---
+++ LeetCode/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+using namespace std;
+
+int main(void){
+    ListNode* l1 = new ListNode(1);
+    ListNode* l2 = new ListNode(2);
+    ListNode* l3 = new ListNode(4);
+    l1->next = l2;
+    l2->next = l3;
+    ListNode* list1 = l1;
+    ListNode* ll1 = new ListNode(1);
+    ListNode* ll2 = new ListNode(3);
+    ListNode* ll3 = new ListNode(4);
+    ll1->next = ll2;
+    ll2->next = ll3;
+    ListNode* list2 = ll1;
+
+    ListNode* list3 = new ListNode();
+    while(list1 && list2){
+        if(!list1 || list1->val > list2->val){
+            list3 = list2;
+            list3 = list3->next;
+            list2 = list2->next;
+        }else if(!list2 || list2->val > list1->val){
+            list3 = list1;
+            list3 = list3->next;
+            list1 = list1->next;
+        }else{
+            list3 = list1;
+            list3->next = list2;
+            list3->next->next = new ListNode();
+        }
+        cout<<list3<<endl;
+    }
+    return 0;
+}
