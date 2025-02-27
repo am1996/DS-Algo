8dd6367ace389640c219ec3da7976053ca9e23e5
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-02-27 21:36:50 +0000

    problem 35 solved

--- LeetCode/35. Search Insert Position.cpp
+++ LeetCode/35. Search Insert Position.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int i=0;
+    for(;i<nums.size();i++){
+        if(nums.at(i) == target) return i;
+        else if(i!=0 && nums.at(i)>target && nums.at(i-1)<target) return i;
+        else if(i== nums.size()-1 && target> nums.at(i)) return i+1;
+    }
+    return 0;
+}
+
+int main(void){
+    vector<int> l = {1,2,3,5};
+    cout<< searchInsert(l,0)<<endl;
+    return 0;
+}
