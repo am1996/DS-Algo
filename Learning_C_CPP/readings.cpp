7820b8671ca1ec498c3bb663916a82c2a9322f6f
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-08 06:04:21 +0000

    reading, write in cpp

--- Learning_C_CPP/readings.cpp
+++ Learning_C_CPP/readings.cpp
@@ -1,10 +1,27 @@
+#include <fstream>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 template <typename... Args> void print(Args... args) {
-  ((std::cout << args << "\n\r"), ...);
+  ((std::cout << args << std::endl), ...);
+  std::cout << "========================================================"
+            << std::endl;
+  for (auto &arg : {args...}) {
+    std::cout << arg << std::endl;
+  }
 }
 
 int main(void) {
-  print(1, 2, 3, 4);
+  std::ofstream f = std::ofstream(
+      "test.txt",
+      std::ios::out | std::ios::binary); // open file  // write to file
+  f << "Hello World" << std::endl;
+  std::ifstream f2 = std::ifstream("test.txt", std::ios::in | std::ios::binary);
+  std::string line;
+  while (std::getline(f2, line)) {
+    std::cout << line << std::endl;
+  }
+  f.close(); // close file
   return 0;
 }
