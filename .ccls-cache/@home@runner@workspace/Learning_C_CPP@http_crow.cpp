a0a96969f466cef0a47bb53064b5b3656a304581
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-13 05:30:58 +0000

    crow

--- .ccls-cache/@home@runner@workspace/Learning_C_CPP@http_crow.cpp
+++ .ccls-cache/@home@runner@workspace/Learning_C_CPP@http_crow.cpp
@@ -1,6 +1,20 @@
+
 #include "crow_all.h"
 
 int main() {
-  crow::SimpleApp app;
-  CROW_ROUTE(app, "/")([]() { return "Hello world"; });
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([]() {
+        return "Hello World!";
+    });
+
+    CROW_ROUTE(app, "/json")([]() {
+        crow::json::wvalue x;
+        x["message"] = "Hello, World!";
+        x["status"] = "success";
+        return x;
+    });
+
+    app.bindaddr("0.0.0.0").port(5000).run();
+    return 0;
 }
