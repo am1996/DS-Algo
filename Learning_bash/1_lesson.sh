4544171de644f368d32de77f25ff5f02a1d10d82
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-16 14:24:13 +0000

    added lesson 1

--- Learning_bash/1_lesson.sh
+++ Learning_bash/1_lesson.sh
@@ -1,10 +1,23 @@
 
-#!/bin/bash
-echo -n "hello world";
+#!/bin/sh
+echo -n "hello world ";
 
 a="hello";
-if [ "$a" = "hello" ]; then
-    echo -n "a is hello";
-elif
-    echo "hello world";
+if [[ "$a" = "hello" ]]; then
+    echo -n "a is hello ";
+elif [[ "$a" = "world" ]]; then
+    echo "hello world ";
 fi
+
+while [ "$a" = "hello" ]; do
+    echo -n "a is hello ";
+    break;
+done
+
+for((i=0;i<=10;i++)); do
+    echo -n "$i";
+done
+
+for i in {1..100..2}; do
+  echo $i;
+done
