4af395ad02b60b867e3c1b68694641631d79545c
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-16 17:29:04 +0000

    lesson  1 end

--- Learning_bash/1_lesson.sh
+++ Learning_bash/1_lesson.sh
@@ -1,5 +1,5 @@
 
-#!/bin/sh
+#!/bin/zsh
 echo -n "hello world ";
 
 a="hello";
@@ -18,6 +18,16 @@ for((i=0;i<=10;i++)); do
     echo -n "$i";
 done
 
-for i in {1..100..2}; do
+for i in {1..10..2}; do
   echo $i;
 done
+
+cat a.txt | while IFS= read -r line; do
+  echo $line | cut -d ' ' -f 1| base64;
+  echo $line | cut -d ' ' -f 2 | base64;
+done
+
+greet(){
+    echo "hello $0 $1";
+}
+greet "hello"
