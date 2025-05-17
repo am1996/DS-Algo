6e0d69b1b2299d175efcea1104d0fb565bf48435
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-17 07:16:20 +0000

    still lesson 3

--- Learning_bash/3_lesson.sh
+++ Learning_bash/3_lesson.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+find / -perm -u=s 2>/dev/null #find all files with suid bit set
+find / -perm -g=s 2>/dev/null #find all files with sgid bit set
 ls -l /proc/35/fd #list all open files by process 35
 ss -tulnp #list all open ports
 ps aux #list all running processes
