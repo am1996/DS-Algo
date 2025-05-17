dd7785eee1a01f4d6d186b2092f588e8ce663998
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-17 09:14:44 +0000

    lesson 3 continue

--- Learning_bash/3_lesson.sh
+++ Learning_bash/3_lesson.sh
@@ -6,3 +6,20 @@ ss -tulnp #list all open ports
 ps aux #list all running processes
 ps ef --forest #list all running processes and subprocesses
 lsof -p 35 #list all open files by process 35
+whoami #print current user
+id #print current user id
+groups #print current user groups
+uname -a #print system information
+lspci #list all pci devices
+lsusb #list all usb devices
+lsblk #list all block devices
+lsmod #list all loaded modules
+modprobe -l #list all available modules
+modprobe -r module_name #remove module
+modprobe module_name #add module
+cat text.txt | sed 's/hello/world/g' #replace occurence of hello with world in text.txt
+cat text.txt | sed '/hello/d' #delete lines with hello in text.txt
+cat text.txt | sed '/hello/p' #print lines with hello in text.txt
+sed text.txt -i 's/hello/world/g' #replace hello with world in place in text.txt
+cat text.txt | grep -in hello #print lines with hello in text.txt with line numbers
+grep -Rin hello / #search for hello in all files in / 
