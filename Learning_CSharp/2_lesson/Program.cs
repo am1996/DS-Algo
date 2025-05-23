6be45eb04b536d88dcf0514e54764611b3e4be29
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-23 20:47:26 +0000

    lesson 2

--- Learning_CSharp/2_lesson/Program.cs
+++ Learning_CSharp/2_lesson/Program.cs
@@ -8,7 +8,7 @@ namespace _2_Lesson;
 class Program{
   public static async Task Main(string[] args){
     using HttpClient client = new HttpClient();
-    string response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1").Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
-    Console.WriteLine(response);
+    string response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/").Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+    
   }
 }
--- Learning_bash/4_lesson.sh
+++ Learning_bash/4_lesson.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-echo -n "hello world";
+#!/bin/zsh
+while IFS= read -r line; do
+    echo "$line \$";
+    sleep 0.05;
+done < 1_lesson.sh
+
+for((i=0;i<11;i++)); do
+    echo $i;
+done
+i="helloworld@gmail.com"
+if [[ "$i" =~ ^[a-zA-Z0-9._%+-]+@[:alphanum:] ]]; then
+    echo "valid email";
+else
+    echo "not valid email";
+fi
