1251dd6a96c48fc4bfca4c3efe5b784245f88e79
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-23 15:54:24 +0000

    added to lesson1

--- Learning_CSharp/1_lesson/Program.cs
+++ Learning_CSharp/1_lesson/Program.cs
@@ -22,6 +22,7 @@ static class CircleExtension{
 class Program{
   public static void Main(string[] args){
     object[] i = [1,2,3,4,"hello",5.5];
+    byte[] bytes = [0x45,0xB,0xC];
     Dictionary<string, string> aq = new Dictionary<string, string> {
         { "1", "hello" },
         { "2", "world" }
@@ -30,10 +31,12 @@ class Program{
     foreach(object j in i){
       Console.WriteLine(j);
     }
+    File.WriteAllText("text.txt","hello world",System.Text.Encoding.UTF8);
     Circle c1 = new(10);
     Circle c2 = new(20);
     Circle c3 = c1+c2;
-    
-    
+    File.WriteAllBytes("textbytes.txt",bytes);
+    byte[] bbb = File.ReadAllBytes("textbytes.txt");
+    Console.WriteLine(bytes);
   }
 }
--- Learning_CSharp/1_lesson/obj/Debug/net9.0/Learning_CSharp.AssemblyInfo.cs
+++ Learning_CSharp/1_lesson/obj/Debug/net9.0/Learning_CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning_CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24a5f037254ba1b49840119ea9a4c2124e8aaeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76912ad0474c2058507dd195a5e14a0e2abfa036")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning_CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning_CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
