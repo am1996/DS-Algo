f24a5f037254ba1b49840119ea9a4c2124e8aaeb
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-23 09:39:05 +0000

    lesson 1 csharp

---
+++ Learning_CSharp/1_lesson/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Diagnostics;
+
+namespace _1_Lesson;
+
+class Program{
+  public static void Main(string[] args){
+    Console.WriteLine("Hello, World!");
+    Process p = Process.Start("bash", "-c echo Hello World");
+    File.WriteAllText("test.txt","hello world");
+  }
+}
