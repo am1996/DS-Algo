3e78218d666d0b79e5b33df41d6beca057bc8ce2
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-24 16:42:28 +0000

    added lesson 3

--- Learning_CSharp/3_lesson/Program.cs
+++ Learning_CSharp/3_lesson/Program.cs
@@ -4,6 +4,11 @@ namespace _3_lesson;
 
 public class Program{
     public static void Main(string[] args){
-        Console.WriteLine("Hello, World!");
+        IEnumerable<int> numbers = Enumerable.Range(1,200);
+        IQueryable<int> numbers2 = numbers.AsQueryable();
+        var result = from number in numbers where number%6==3 select number;
+        foreach(int i in result){
+          Console.WriteLine(i);
+        }
     }
 }
