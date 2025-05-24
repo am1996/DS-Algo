01557636b8d8c637ef2446d57cba01f3d2d4ee52
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-24 08:11:03 +0000

    bs4 like in csharp

--- Learning_CSharp/2_lesson/Program.cs
+++ Learning_CSharp/2_lesson/Program.cs
@@ -2,6 +2,9 @@ using System;
 using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
+using HtmlAgilityPack;
+using System;
+using System.Linq;
 
 namespace _2_Lesson;
 record Todo(int userId, int id, string title, bool completed);
@@ -11,5 +14,12 @@ class Program{
     string response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/").Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
     List<Todo> todo = JsonSerializer.Deserialize<List<Todo>>(response);
     Console.WriteLine(todo[0]);
+    string response2 = await client.GetAsync("https://wuzzuf.net/search/jobs/?q=web%20developer&a=hpb").Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+    var htmlDoc = new HtmlDocument();
+    htmlDoc.LoadHtml(response2);
+    var jobs = htmlDoc.DocumentNode.SelectNodes("//div[@class='css-1gatmva e1v1l3u10']");
+    foreach(var job in jobs){
+      Console.WriteLine(job.InnerText);
+    }
   }
 }
