5bdba797e831ca860bcd000ed42ec692bcdb6eea
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-24 09:15:03 +0000

    sync logger

---
+++ Learning_CSharp/2_lesson/Logger.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public class AsyncLogger : IDisposable
+{
+    private StreamWriter _writer;
+
+    public AsyncLogger(string filePath)
+    {
+        _writer = new StreamWriter(filePath, append: true)
+        {
+            AutoFlush = true
+        };
+    }
+
+    public async Task LogAsync(string message)
+    {
+        await _writer.WriteLineAsync($"{DateTime.Now}: {message}");
+    }
+
+    public void Dispose()
+    {
+        _writer?.Dispose();
+    }
+}
