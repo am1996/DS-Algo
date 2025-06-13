6b0d4d2ef5ddf8debeb5e53b8ee4abca39f945c6
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-06-13 11:24:02 +0000

    added lesson 6

--- Learning_bash/5_lesson.sh
+++ Learning_bash/5_lesson.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 ls | xargs -I {} sh -c 'echo "{} $(wc -l < {})"'
 ls | xargs -I {} sh -c 'echo "{} $(cat {})"'
+find / -name "*.log" 2>/dev/null | xargs -I {} sh -c 'cat {} | grep -i "password"' #search for password
