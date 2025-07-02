6ab4d44d47c9a06f5327258a8b825d53e0cd29d7
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-07-02 12:07:11 +0300

    done

--- learning_powershell/1st_lesson.ps1
+++ learning_powershell/1st_lesson.ps1
@@ -5,3 +5,11 @@ $a = $(Get-Process | Select-Object -Last 5)
 foreach($process in $a){
     Write-Host "Process Name: $($process.Name) - ID: $($process.Id)" -ForegroundColor Green
 }
+for($b = 0; $b -lt 5; $b++){
+    Write-Host "This is iteration number: $b" -ForegroundColor Yellow
+}
+$b = 0;
+do{
+    Write-Host "This is a do-while loop iteration number: $b" -ForegroundColor Cyan
+    $b++
+}while($b -lt 5)
