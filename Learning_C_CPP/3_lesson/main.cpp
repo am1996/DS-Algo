d522232a1d05d1dfbd894ef60dd068c59300d63d
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-07-04 16:25:41 +0000

    done

--- Learning_C_CPP/3_lesson/main.cpp
+++ Learning_C_CPP/3_lesson/main.cpp
@@ -1,23 +1,14 @@
 
 #include "Crow/include/crow.h"
 
-int main()
-{
-    crow::SimpleApp app;
+int main() {
+  crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
-    });
+  CROW_ROUTE(app, "/hehe")
+  ([]() {
+    auto page = crow::mustache::load_text("./templates/index.html");
+    return page;
+  });
 
-    CROW_ROUTE(app, "/json")([]{
-        crow::json::wvalue x;
-        x["message"] = "Hello, World!";
-        return x;
-    });
-
-    CROW_ROUTE(app, "/hello/<string>")([](const std::string& name){
-        return "Hello " + name + "!";
-    });
-
-    app.port(5000).multithreaded().run();
+  app.port(5000).multithreaded().run();
 }
