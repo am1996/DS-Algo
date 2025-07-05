54f9b4cee6937112cee944ffab7047f36e167e49
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-07-05 20:25:42 +0000

    done

--- .nix
+++ .nix
@@ -1,15 +1,11 @@
 
 { pkgs ? import <nixpkgs> {} }:
-pkgs.buildInputs = [
-  pkgs.openssl
-];
 pkgs.mkShell {
   buildInputs = with pkgs; [
     asio
     gcc
     sqlite
     pkg-config
-    powershell
     sqlite.dev
     cmake
   ];
--- Learning_C_CPP/3_lesson/main.cpp
+++ Learning_C_CPP/3_lesson/main.cpp
@@ -1,10 +1,24 @@
 
 #include "Crow/include/crow.h"
-#include <jwt-cpp/jwt.h> // Include the jwt-cpp library
+#include <sqlite3.h>
 
 int main() {
   crow::SimpleApp app;
-
+  CROW_ROUTE(app, "/register")
+  ([](const crow::request req) {
+    auto x = crow::json::load(req.body);
+    if (!x)
+      return crow::response(400);
+    std::string username = x["username"].s();
+    std::string password = x["password"].s();
+    sqlite3 *db;
+    sqlite3_open("database.db", &db);
+    std::string sql = "INSERT INTO users (username, password) VALUES ('" +
+                      username + "', '" + password + "');";
+    sqlite3_exec(db, sql.c_str(), NULL, NULL, NULL);
+    sqlite3_close(db);
+    return crow::response(200, "User Created Successfully");
+  });
   CROW_ROUTE(app, "/")
   ([](const crow::request req) {
     auto page = crow::mustache::load("index.html");
