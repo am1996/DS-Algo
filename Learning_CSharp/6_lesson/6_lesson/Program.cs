dbed0fac0bdf6790408285262376924a71070b4e
am1996 <ahmedmagdy92yahoo.com>
2025-08-08 00:08:18 +0300

    a

---
+++ Learning_CSharp/6_lesson/6_lesson/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _6_lesson;
+
+class Program
+{
+    [DllImport("kernel32.dll")]
+    private static extern int CreateProcess
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ Learning_C_CPP/md_l3/md_l3/md_l3.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
