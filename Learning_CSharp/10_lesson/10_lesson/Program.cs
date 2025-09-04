97cdb5e0cccddef7631d896a4aa54a7f36777d55
am1996 <ahmedmagdy92yahoo.com>
2025-09-04 17:00:15 +0300

    ddd

---
+++ Learning_CSharp/10_lesson/10_lesson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Program;
+
+class Program
+{
+    [DllImport("user32.dll", CharSet = CharSet.Ansi)]
+    private static extern int MessageBoxA(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+    public static int Main(string[] args)
+    {
+        MessageBoxA(IntPtr.Zero, "hello world", "This is me", 0x00000000 | 0x00000040);
+        Console.WriteLine("hello world");
+        return 0;
+    }
+}
