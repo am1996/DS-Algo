a0e1cb32c0ae2fd578856644a3a5a0f9e853281f
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-12-13 18:35:59 +0000

    leet code 16 solved but needs optimization

--- LeetCode/16. 3sum closest.py
+++ LeetCode/16. 3sum closest.py
@@ -1,21 +1,24 @@
-target = 4
+target = 1
 nums = [-1, 2, 1, -4]
-nums.sort()
-print(nums)
+
 
 def Closest_3_Numbers(nums, target):
   nums.sort()
-  closest = nums[0] + nums[1] + nums[2]
+  closest_sum = nums[0] + nums[1] + nums[2]
   for i in range(len(nums) - 2):
     left = i + 1
     right = len(nums) - 1
     while left < right:
       sum = nums[i] + nums[left] + nums[right]
-      if abs(sum - target) < abs(closest - target):
-        closest = sum
+      if abs(target - sum) < abs(target - closest_sum):
+        closest_sum = sum
       if sum < target:
         left += 1
       elif sum > target:
         right -= 1
       else:
-        return sum
+        return closest_sum
+  return closest_sum
+
+
+print(Closest_3_Numbers(nums, 1))
