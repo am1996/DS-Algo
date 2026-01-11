61af8fb4f12888648aec4d5dff57e3c642f3a3ee
am1996 <ahmedmagdy92yahoo.com>
2026-01-11 22:36:05 +0200

    learning nim

--- learning_nim/lec_6/main.nim
+++ learning_nim/lec_6/main.nim
@@ -1,12 +1,21 @@
 import sequtils,sugar
 import winim
 
+proc filterer(numbers: seq[int] | array[4, int], predicate: (int)-> bool): seq[int] =
+    var r: seq[int] = @[]
+    for num in numbers:
+        if predicate(num):
+            r.add(num)
+    return r
+
 proc main()=
     let numbers: seq[int] = @[1,2,3,4]
-    let oddNumbers = filter(numbers, (x:int)->bool => x mod 2 != 0)
+    let oddNumbers = filterer(numbers, (x:int)->bool => x mod 2 != 0)
     echo oddNumbers
     let nimm: array[4, int] = [1,2,3,4]
-    let oddNim = filter(nimm, (x)=> x mod 2 != 0)
+    let oddNim = filterer(nimm, (x)=> x mod 2 != 0)
     echo oddNim
+    let isLowerCase = {'A'..'Z'}
+    echo isLowerCase
 if isMainModule:
     main()
